557ff19d457c778d22a5c187348b5dadcfa0e6c1
BlackGoku36 <urjasvisuthar@gmail.com>
2020-06-26 00:20:15 +0530

    shader fix

--- Shaders/shade.vert.glsl
+++ Shaders/shade.vert.glsl
@@ -1,7 +1,7 @@
 #version 450
 
-in vec3 pos;
-in vec3 nor;
+in vec4 pos;
+in vec4 nor;
 
 out vec3 normal;
 out vec3 fragPos;
@@ -10,7 +10,7 @@ uniform mat4 MVP;
 uniform mat4 M;
 
 void main() {
-    gl_Position = MVP * vec4(pos, 1.0);
-    fragPos = vec3(M * vec4(pos, 1.0));
-    normal = nor;
+    gl_Position = MVP * vec4(pos.xyz, 1.0);
+    fragPos = vec3(M * vec4(pos.xyz, 1.0));
+    normal = vec3(nor.xy, pos.w);
 }
