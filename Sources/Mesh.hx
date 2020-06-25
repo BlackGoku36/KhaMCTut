985dd0668f33ee322a8849dc1d75a4120f89fc37
BlackGoku36 <urjasvisuthar@gmail.com>
2020-06-26 00:10:19 +0530

    attempt

--- Sources/Mesh.hx
+++ Sources/Mesh.hx
@@ -1,5 +1,7 @@
 package;
 
+import kha.arrays.Uint32Array;
+import kha.arrays.Int16Array;
 import kha.graphics4.Graphics;
 import kha.FastFloat;
 import kha.Shaders;
@@ -19,7 +21,7 @@ class Mesh {
 	static var vertexBuffer:VertexBuffer;
 	static var indexBuffer:IndexBuffer;
 	static var structure: VertexStructure;
-	var structureLength = 6;
+	var structureLength = 0;
 
 	var pipeline:PipelineState;
 
@@ -52,8 +54,9 @@ class Mesh {
 	public function load() {
 
 		structure = new VertexStructure();
-		structure.add("pos", VertexData.Float3);
-		structure.add("nor", VertexData.Float3);
+		structure.add("pos", VertexData.Short4Norm);
+		structure.add("nor", VertexData.Short2Norm);
+		structureLength = Std.int(structure.byteSize()/4);
 
 		pipeline = new PipelineState();
 		pipeline.inputLayout = [structure];
@@ -75,24 +78,28 @@ class Mesh {
 		lightPositionC = pipeline.getConstantLocation("lightPosition");
 		lightColorC = pipeline.getConstantLocation("lightColor");
 
-		vertexBuffer = new VertexBuffer(Std.int(vertices.length / 3)*2, structure, Usage.StaticUsage);
+		for(i in 0...vertices.length){
+			vertices[i] = vertices[i] * (1 / 32);
+		}
 
-		var vbData = vertexBuffer.lock();
+		var convert = convert();
+		vertexBuffer = new VertexBuffer(Std.int(convert.verts.length / 4), structure, Usage.StaticUsage);
+		var vbData = vertexBuffer.lockInt16();
 		for (i in 0...Std.int(vbData.length / structureLength)) {
-			vbData.set(i * structureLength, vertices[i * 3]);
-			vbData.set(i * structureLength + 1, vertices[i * 3 + 1]);
-			vbData.set(i * structureLength + 2, vertices[i * 3 + 2]);
-			vbData.set(i * structureLength + 3, normals[i * 3]);
-			vbData.set(i * structureLength + 4, normals[i * 3 + 1]);
-			vbData.set(i * structureLength + 5, normals[i * 3 + 2]);
+			vbData.set(i * structureLength, convert.verts[i * 4]);
+			vbData.set(i * structureLength + 1, convert.verts[i * 4 + 1]);
+			vbData.set(i * structureLength + 2, convert.verts[i * 4 + 2]);
+			vbData.set(i * structureLength + 3, convert.verts[i * 4 + 3]);
+			vbData.set(i * structureLength + 4, convert.nor[i * 2]);
+			vbData.set(i * structureLength + 5, convert.nor[i * 2+ 1]);
 		}
 		vertexBuffer.unlock();
 
-		indexBuffer = new IndexBuffer(indices.length*2, Usage.StaticUsage);
+		indexBuffer = new IndexBuffer(convert.ind.length, Usage.StaticUsage);
 
 		var iData = indexBuffer.lock();
 		for (i in 0...iData.length) {
-			iData[i] = indices[i];
+			iData[i] = convert.ind[i];
 		}
 		indexBuffer.unlock();
 	}
@@ -135,5 +142,36 @@ class Mesh {
 		g.setVector3(lightColorC, lightColor);
 		g.drawIndexedVertices();
 		g.end();
+	}
+
+	function convert() {
+		var numVertices = Std.int(vertices.length / 3);
+		var posI16 = new Int16Array(numVertices * 4); // pos.xyz, nor.z
+		var norI16 = new Int16Array(numVertices * 2); // nor.xy
+		toI16(posI16, norI16, vertices, normals);
+
+		var indU32 = new Uint32Array(indices.length);
+		toU32(indU32, indices);
+
+		return{verts:posI16, nor:norI16, ind:indU32}
+	}
+
+	function toI16(toPos:Int16Array, toNor:Int16Array, fromPos:Array<Float>, fromNor:Array<Float>) {
+		var numVertices = Std.int(fromPos.length / 3);
+		for (i in 0...numVertices) {
+			// Values are scaled to the signed short (-32768, 32767) range
+			// In the shader, vertex data is normalized into (-1, 1) range
+			toPos[i * 4    ] = Std.int(fromPos[i * 3    ] * 32767);
+			toPos[i * 4 + 1] = Std.int(fromPos[i * 3 + 1] * 32767);
+			toPos[i * 4 + 2] = Std.int(fromPos[i * 3 + 2] * 32767);
+			toNor[i * 2    ] = Std.int(fromNor[i * 3    ] * 32767);
+			toNor[i * 2 + 1] = Std.int(fromNor[i * 3 + 1] * 32767);
+			// normal.z component is packed into position.w component
+			toPos[i * 4 + 3] = Std.int(fromNor[i * 3 + 2] * 32767);
+		}
+	}
+
+	function toU32(to:Uint32Array, from:Array<Int>) {
+		for (i in 0...to.length) to[i] = from[i];
 	}
 }
